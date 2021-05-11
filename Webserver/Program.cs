5260f5496e2829a8b3df51d087ac5010a1c19dae
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-05-11 15:47:52 +0200

    Projektdateien hinzufügen.

---
+++ Webserver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Webserver {
+
+    public class GetSocket {
+
+        public static void Main(String[] args) {
+            var server = new MyWebserver();
+        }
+    }
+}
